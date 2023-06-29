fa68f71c4e2e80cf16ccdfc75ff1d89db077d51d
iseolin <iseolin76@quantumuniverse.co>
2023-06-29 18:00:23 +0900

    Create: Articles component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
+import Articles from "@/components/articles";
 import Form from "@/components/form";
 
 const Home = () => {
   return (
     <main className="flex justify-between gap-5">
       <Form />
-      <div className="flex-1">list 칸</div>
+      <Articles />
     </main>
   );
 };
---
+++ src/components/articles/index.tsx
@@ -0,0 +1,12 @@
+const Articles = () => {
+  return (
+    <div className="flex-1">
+      <ul className="grid gap-4 grid-cols-1">
+        <li className="px-4 py-2 border rounded-lg border-yellow-300">asdf</li>
+        <li className="px-4 py-2 border rounded-lg border-yellow-300">asdf</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Articles;
